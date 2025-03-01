5c46b265cb1ac9cf127cb0639a8023e2ae069c5b
iSoFe2 <gbgbmmm9@gmail.com>
2025-03-02 01:08:34 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-import react from "@vitejs/plugin-react";
-import {defineConfig} from "vite";
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    allowedHosts: true,
-  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  }
 });
